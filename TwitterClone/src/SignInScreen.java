ab3b6dd8b135607f72df0dfc20e1d5d80ee020eb
Tuba Asif <68794712+tuba-theProgrammer@users.noreply.github.com>
2022-09-09 01:55:36 +0500

    designed WelcomeScreen

--- TwitterClone/src/SignInScreen.java
+++ TwitterClone/src/SignInScreen.java
@@ -4,37 +4,46 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
 
 public class SignInScreen extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField usernameField;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SignInScreen frame = new SignInScreen();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
-	/**
-	 * Create the frame.
-	 */
 	public SignInScreen() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBackground(new Color(192, 192, 192));
+		setTitle("Login Screen");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setVisible(true);
+		setBounds(100, 100, 902, 604);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel imgLabel = new JLabel("imgLabel");
+		imgLabel.setBounds(408, 107, 70, 15);
+		contentPane.add(imgLabel);
+		
+		JLabel lblLoginToTwitter = new JLabel("Login To Twitter");
+		lblLoginToTwitter.setFont(new Font("Dialog", Font.BOLD, 20));
+		lblLoginToTwitter.setBounds(353, 173, 307, 29);
+		contentPane.add(lblLoginToTwitter);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setFont(new Font("Dialog", Font.BOLD, 17));
+		lblUsername.setBounds(115, 230, 307, 29);
+		contentPane.add(lblUsername);
+		
+		usernameField = new JTextField();
+		usernameField.setBounds(125, 271, 611, 44);
+		contentPane.add(usernameField);
+		usernameField.setColumns(10);
 	}
-
 }
---
+++ TwitterClone/src/WelcomeScreen.java
@@ -0,0 +1,78 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class WelcomeScreen extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Create the frame.
+	 * @throws IOException 
+	 */
+	public WelcomeScreen() {
+		setBackground(new Color(128, 128, 128));
+		setVisible(true);
+		setTitle("Welcome screen");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 902, 604);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(30, 144, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel imgLabel = new JLabel("imgLabel");
+		imgLabel.setBounds(409, 71, 70, 15);
+		contentPane.add(imgLabel);
+		
+		JLabel lblWelcome = new JLabel("Welcome To Twitter");
+		lblWelcome.setFont(new Font("Dialog", Font.BOLD, 22));
+		lblWelcome.setBounds(323, 176, 307, 29);
+		contentPane.add(lblWelcome);
+		
+		JButton loginBtn = new JButton("Login ");
+		loginBtn.setBackground(new Color(0, 0, 128));
+		loginBtn.setForeground(new Color(255, 255, 255));
+		loginBtn.setFont(new Font("Dialog", Font.BOLD, 18));
+		loginBtn.setBounds(335, 262, 244, 40);
+		contentPane.add(loginBtn);
+		
+		loginBtn.addActionListener(new ActionListener(){
+
+	         //defining a method actionPerformed
+	        public void actionPerformed(ActionEvent ae){ 
+	        	new SignInScreen();
+	        }});
+		
+		JButton btnCreateAccount = new JButton("Create Account");
+		btnCreateAccount.setForeground(Color.WHITE);
+		btnCreateAccount.setFont(new Font("Dialog", Font.BOLD, 18));
+		btnCreateAccount.setBackground(new Color(0, 0, 128));
+		btnCreateAccount.setBounds(335, 345, 244, 40);
+		contentPane.add(btnCreateAccount);
+        
+		
+		
+		btnCreateAccount.addActionListener(new ActionListener(){
+
+	         //defining a method actionPerformed
+	        public void actionPerformed(ActionEvent ae){ 
+	        	
+	        }});
+	}
+}
